3d6ceae1526bff11e6d01656a7d7c22c29c75471
saki <saki.0605.daruma@gmail.com>
2021-01-03 12:55:39 +0000

    first

---
+++ test-carousel.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+      {
+           loop: true,
+            nav: false,
+            dots: false,
+            items: 1,
+            autoplay: true
+          
+          
+      }  
+        
+    
+        
+        );
+});
+
+
+
+
+
+
+$(function(){
+	$(window).on('load scroll',function (){
+		$('.animation').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+});
